967888717e6dca3dea0542f1ee8547c1554099c9
yusufhidayah <yusuf.hidayah@bukalapak.com>
2015-10-28 15:18:23 +0700

    change visitors_controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,10 @@
 class VisitorsController < ApplicationController
 
   def new
+    Rails.logger.debug 'Debug: entering new method'
     @owner = Owner.new
-    render 'visitors/new'
+    Rails.logger.debug 'Debug: Owner name is '+@owner.name
+    render "visitors/new"
   end
 
 end
